c6e0cf2a924be19c10f18453c1017e61f9f2b4e8
Miguel Bayon <miguel.bayon@iessanandres.com>
2019-10-08 09:40:57 +0200

    Ahora se muestra el año de nacimiento

--- Prueba.java
+++ Prueba.java
@@ -3,5 +3,6 @@ public class Prueba {
         System.out.println("Iván García");
         System.out.println("León");
         System.out.println("León");
+        System.out.println("1996");
     }
 }
